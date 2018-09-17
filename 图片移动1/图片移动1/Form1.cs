2571a1c20cbbc7602eea43331487e322f79b53ec
izzp <zzpi@vip.qq.com>
2018-09-17 11:58:54 +0800

    修改暂停键

--- 图片移动1/图片移动1/Form1.cs
+++ 图片移动1/图片移动1/Form1.cs
@@ -58,14 +58,8 @@ namespace 图片移动1
 
         private void button3_Click(object sender, EventArgs e)///暂停
         {
-            if (timer1.Enabled = true) 
-            {
                 timer1.Stop();  
-            }
-            else if (timer2.Enabled = true)
-            {
                 timer2.Stop();
-            }
         }
     
         private void button4_Click(object sender, EventArgs e)///加速
