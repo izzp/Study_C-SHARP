4122ffa2829d3d1e1c914688af726636853924fd
izzp <zzpi@vip.qq.com>
2018-09-17 16:49:34 +0800

    优化注释内容

--- 图片移动1/图片移动1/Form1.Designer.cs
+++ 图片移动1/图片移动1/Form1.Designer.cs
@@ -156,6 +156,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "图片移动";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- 图片移动1/图片移动1/Form1.cs
+++ 图片移动1/图片移动1/Form1.cs
@@ -18,26 +18,26 @@ namespace 图片移动1
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)///左右移动按钮
+        private void button1_Click(object sender, EventArgs e)//左右移动按钮
         {
             timer2.Enabled = false;
             timer3.Enabled = false;
             timer1.Enabled = true;
         }
-        private void button2_Click(object sender, EventArgs e)///上下移动按钮
+        private void button2_Click(object sender, EventArgs e)//上下移动按钮
         {
             timer1.Enabled = false;
             timer3.Enabled = false;
             timer2.Enabled = true;
         }
-        private void button7_Click(object sender, EventArgs e)///环绕运动按钮
+        private void button7_Click(object sender, EventArgs e)//环绕运动按钮
         {
             timer1.Enabled = false;
             timer2.Enabled = false;
             timer3.Enabled = true;          
         }
 
-        private void timer1_Tick(object sender, EventArgs e)///左右运动
+        private void timer1_Tick(object sender, EventArgs e)//左右运动
         {      
             pictureBox1.Left += v * direct;
             if (pictureBox1.Right > this.Width)
@@ -51,7 +51,7 @@ namespace 图片移动1
 
         }
 
-        private void timer2_Tick(object sender, EventArgs e)///上下运动
+        private void timer2_Tick(object sender, EventArgs e)//上下运动
         {        
             pictureBox1.Top += v * direct;
             if (pictureBox1.Bottom > this.Height)
@@ -63,25 +63,25 @@ namespace 图片移动1
                 direct = 1;
             }
         }
-        private void timer3_Tick(object sender, EventArgs e)///环绕运动
+        private void timer3_Tick(object sender, EventArgs e)//环绕运动
         {
             while (pictureBox1.Right < this.Width)
             {
                 pictureBox1.Left += v * direct;
              }
         }
-        private void button3_Click(object sender, EventArgs e)///暂停
+        private void button3_Click(object sender, EventArgs e)//暂停
         {
                 timer1.Stop();  
                 timer2.Stop();
         }
     
-        private void button4_Click(object sender, EventArgs e)///加速
+        private void button4_Click(object sender, EventArgs e)//加速
         {
             v += 10;
         }
 
-        private void button5_Click(object sender, EventArgs e)///减速
+        private void button5_Click(object sender, EventArgs e)//减速
         {
             if (v > 5)
             {
@@ -89,13 +89,18 @@ namespace 图片移动1
             }
         }
 
-        private void button6_Click(object sender, EventArgs e)///复位
+        private void button6_Click(object sender, EventArgs e)//复位
         {
             timer1.Enabled = false;
             timer2.Enabled = false;
             pictureBox1.Top = 0;
             pictureBox1.Left = 0;
             v = 10;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
