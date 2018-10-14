5d766946bd9051b12eaf22ff5904cd613091ec49
izzp <zzpi@vip.qq.com>
2018-10-15 01:39:44 +0800

    初步交通灯

--- W模拟交通灯/WSimulatedTrafficLight/Form1.Designer.cs
+++ W模拟交通灯/WSimulatedTrafficLight/Form1.Designer.cs
@@ -28,19 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.菜单ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.textBoxRedLight = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.btnBegin = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.RednumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.GreennumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.YellonumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RednumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreennumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellonumericUpDown)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -56,9 +65,18 @@
             // 
             // 菜单ToolStripMenuItem
             // 
+            this.菜单ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ExitToolStripMenuItem});
             this.菜单ToolStripMenuItem.Name = "菜单ToolStripMenuItem";
             this.菜单ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
             this.菜单ToolStripMenuItem.Text = "菜单";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.ExitToolStripMenuItem.Text = "退出";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.EixtToolStripMenuItem_Click);
             // 
             // AboutToolStripMenuItem
             // 
@@ -67,32 +85,10 @@
             this.AboutToolStripMenuItem.Text = "关于";
             this.AboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
             // 
-            // textBoxRedLight
-            // 
-            this.textBoxRedLight.Location = new System.Drawing.Point(356, 64);
-            this.textBoxRedLight.Name = "textBoxRedLight";
-            this.textBoxRedLight.Size = new System.Drawing.Size(100, 21);
-            this.textBoxRedLight.TabIndex = 1;
-            this.textBoxRedLight.TextChanged += new System.EventHandler(this.textBoxRedLight_TextChanged);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(356, 102);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
-            this.textBox2.TabIndex = 2;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(356, 138);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 21);
-            this.textBox3.TabIndex = 3;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(273, 69);
+            this.label1.Location = new System.Drawing.Point(273, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(77, 12);
             this.label1.TabIndex = 4;
@@ -101,7 +97,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(273, 106);
+            this.label2.Location = new System.Drawing.Point(273, 112);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(77, 12);
             this.label2.TabIndex = 5;
@@ -110,7 +106,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(273, 143);
+            this.label3.Location = new System.Drawing.Point(273, 148);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(77, 12);
             this.label3.TabIndex = 6;
@@ -118,42 +114,112 @@
             // 
             // btnBegin
             // 
-            this.btnBegin.Location = new System.Drawing.Point(275, 204);
+            this.btnBegin.Location = new System.Drawing.Point(263, 215);
             this.btnBegin.Name = "btnBegin";
             this.btnBegin.Size = new System.Drawing.Size(75, 23);
             this.btnBegin.TabIndex = 7;
             this.btnBegin.Text = "开始\r\n";
             this.btnBegin.UseVisualStyleBackColor = true;
+            this.btnBegin.Click += new System.EventHandler(this.btnBegin_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(54, 27);
+            this.pictureBox1.Image = global::WSimulatedTrafficLight.Properties.Resources.nrgy;
+            this.pictureBox1.Location = new System.Drawing.Point(27, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 214);
+            this.pictureBox1.Size = new System.Drawing.Size(131, 254);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(277, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "亮 灯 方 式 ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "红灯先亮",
+            "绿灯先亮",
+            "黄灯先亮"});
+            this.comboBox1.Location = new System.Drawing.Point(372, 177);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 20);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "请选择";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // RednumericUpDown
+            // 
+            this.RednumericUpDown.Location = new System.Drawing.Point(372, 69);
+            this.RednumericUpDown.Name = "RednumericUpDown";
+            this.RednumericUpDown.Size = new System.Drawing.Size(100, 21);
+            this.RednumericUpDown.TabIndex = 11;
+            // 
+            // GreennumericUpDown
+            // 
+            this.GreennumericUpDown.Location = new System.Drawing.Point(372, 106);
+            this.GreennumericUpDown.Name = "GreennumericUpDown";
+            this.GreennumericUpDown.Size = new System.Drawing.Size(100, 21);
+            this.GreennumericUpDown.TabIndex = 12;
+            this.GreennumericUpDown.ValueChanged += new System.EventHandler(this.YellonumericUpDown_ValueChanged);
+            // 
+            // YellonumericUpDown
+            // 
+            this.YellonumericUpDown.Location = new System.Drawing.Point(373, 143);
+            this.YellonumericUpDown.Name = "YellonumericUpDown";
+            this.YellonumericUpDown.Size = new System.Drawing.Size(99, 21);
+            this.YellonumericUpDown.TabIndex = 13;
+            this.YellonumericUpDown.ValueChanged += new System.EventHandler(this.GreennumericUpDown_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 215);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "重置";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(513, 284);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.YellonumericUpDown);
+            this.Controls.Add(this.GreennumericUpDown);
+            this.Controls.Add(this.RednumericUpDown);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnBegin);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBoxRedLight);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "模拟交通灯";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RednumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreennumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellonumericUpDown)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,9 +228,6 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.TextBox textBoxRedLight;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -172,6 +235,14 @@
         private System.Windows.Forms.ToolStripMenuItem 菜单ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown RednumericUpDown;
+        private System.Windows.Forms.NumericUpDown GreennumericUpDown;
+        private System.Windows.Forms.NumericUpDown YellonumericUpDown;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- W模拟交通灯/WSimulatedTrafficLight/Form1.cs
+++ W模拟交通灯/WSimulatedTrafficLight/Form1.cs
@@ -15,19 +15,151 @@ namespace WSimulatedTrafficLight
         {
             InitializeComponent();
         }
-
+        int red_time, yellow_time, green_time, time = 0;  //定义红绿灯亮的时间
         private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show ("雨心Dream \n 项目地址：https://github.com/izzp/Study_C-SHARP");
+            MessageBox.Show("雨心Dream ");
         }
 
         private void textBoxRedLight_TextChanged(object sender, EventArgs e)
         {
-    
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("请选择亮灯方式");
+            }
+            else
+                switch (this.comboBox1.SelectedItem.ToString())
+                {
+                    case "红灯先亮":
+                        {
+                            if (red_time > time)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.r;
+                                time++;
+                                Text = "当前红灯亮了" + time + "秒";
+                            }
+                            else if (green_time > time - red_time && green_time > 0)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.g;
+                                time++;
+                                Text = "当前绿灯亮了" + (time - red_time) + "秒";
+                            }
+                            else if (yellow_time + green_time + red_time > time && yellow_time > 0)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.y;
+                                time++;
+                                Text = "当前黄灯亮了" + (time - red_time - green_time) + "秒";
+                            }
+                            else
+                            {
+                                time = 0;
+                            };
+                            break;
+                        }
+                    case "绿灯先亮":
+                        {
+                            if (green_time > time)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.g;
+                                time++;
+                                Text = "当前绿灯亮了" + time + "秒";
+                            }
+                            else if (yellow_time > time - green_time && yellow_time > 0)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.y;
+                                time++;
+                                Text = "当前黄灯亮了" + (time - green_time) + "秒";
+                            }
+                            else if (yellow_time + green_time + red_time > time && red_time > 0)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.r;
+                                time++;
+                                Text = "当前红灯亮了" + (time - yellow_time - green_time) + "秒";
+                            }
+                            else
+                            {
+                                time = 0;
+                            };
+                            break;
+                        }
+                    case "黄灯先亮":
+                        {
+                            if (yellow_time > time)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.y;
+                                time++;
+                                Text = "当前黄灯亮了" + time + "秒";
+                            }
+                            else if (red_time > time - red_time && green_time > 0)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.r;
+                                time++;
+                                Text = "当前红灯亮了" + (time - yellow_time) + "秒";
+                            }
+                            else if (yellow_time + green_time + red_time > time && green_time > 0)
+                            {
+                                pictureBox1.Image = WSimulatedTrafficLight.Properties.Resources.g;
+                                time++;
+                                Text = "当前绿灯亮了" + (time - red_time - yellow_time) + "秒";
+                            }
+                            else
+                            {
+                                time = 0;
+                            };
+                            break;
+                        }
+                    default:
+                        break;
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
 
+        private void EixtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
 
+        private void btnBegin_Click(object sender, EventArgs e)
+        {
+            red_time = (int)this.RednumericUpDown.Value;
+            yellow_time = (int)this.YellonumericUpDown.Value;
+            green_time = (int)this.GreennumericUpDown.Value;     //获取时间
+            timer1.Enabled = true;
+        }
 
+        private void RednumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GreennumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void YellonumericUpDown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
