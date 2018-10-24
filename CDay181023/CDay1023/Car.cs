701a1d98fd1d85cd714445cacf5c251b5e2e1c8b
izzp <zzpi@vip.qq.com>
2018-10-24 10:30:07 +0800

    新建类，类的使用

---
+++ CDay181023/CDay1023/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CDay1023
+{
+    class Car
+    {
+        public string  chePai;
+        public string name;
+        public string color;
+        public int  money;
+        public string carName;
+
+        public  void red()
+        {
+            Console.WriteLine("车牌号{0}闯红灯了，扣6分", chePai);
+        }
+
+        public  void moneyBuy()
+        {
+            Console.WriteLine("我的{0}车买的时候{1}钱", name, money);
+        }
+        public  void desploy()
+        {
+            Console.WriteLine("车牌是{0}，品牌是{1}，颜色是{2}",chePai ,carName ,color );
+        }
+        
+    }
+}
---
+++ CDay181023/CDay1023/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CDay1023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car haHa=new Car() ;
+            haHa.chePai = "冀A88888";
+            haHa .carName="别克君威";
+            haHa.color = "红色";
+            haHa.money = 200000;
+            haHa.red();
+            haHa.desploy();
+            Car haH = new Car();
+            haH.chePai = "冀A66666";
+            haH.carName  = "大众迈腾";
+            haH.color = "银色";
+            haH.money = 180000;
+            haH.red();
+            haH.desploy();
+        }
+    }
+}
