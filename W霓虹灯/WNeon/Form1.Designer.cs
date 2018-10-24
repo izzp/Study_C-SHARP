698de05f8334be3f765f571e890c3be2050f2ac7
izzp <zzpi@vip.qq.com>
2018-10-24 19:12:39 +0800

    霓虹灯开坑

---
+++ W霓虹灯/WNeon/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WNeon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBorn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(28, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(443, 227);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnBorn
+            // 
+            this.btnBorn.Location = new System.Drawing.Point(37, 13);
+            this.btnBorn.Name = "btnBorn";
+            this.btnBorn.Size = new System.Drawing.Size(124, 23);
+            this.btnBorn.TabIndex = 1;
+            this.btnBorn.Text = "动态生成label";
+            this.btnBorn.UseVisualStyleBackColor = true;
+            this.btnBorn.Click += new System.EventHandler(this.btnBorn_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(227, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 30);
+            this.label1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 307);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBorn);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "霓虹灯";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBorn;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ W霓虹灯/WNeon/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WNeon
+{
+    public partial class Form1 : Form
+    {
+        Color[] colors = new Color[5] { Color.Cyan, Color.Red, Color.Pink, Color.Yellow, Color.Green };//颜色数组
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBorn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Label lab1 = new Label();
+                lab1.BackColor = colors[i];
+                lab1.Size = new Size(30, 30);
+                lab1.Location = new Point(30*i, 0);
+                panel1.Controls.Add(lab1);
+            }
+        }
+    }
+}
