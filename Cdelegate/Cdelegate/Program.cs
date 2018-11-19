3007e9ecfe9e8f95513b0c8b8c2f984bf5a46ef0
雨心 <i@mezzp.com>
2018-11-19 15:35:26 +0800

    委托

---
+++ Cdelegate/Cdelegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cdelegate
+{
+    class Program
+    {
+        delegate int haha(int a, int b);
+        static void Main(string[] args)
+        {
+            Class1 c=new Class1();
+            haha ha=new haha(c.add);
+            Console.WriteLine(ha(5,7));
+
+        }
+    }
+}
