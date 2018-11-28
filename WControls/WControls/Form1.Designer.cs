04f474c151b7d575156026b839d19d949fd78c73
雨心 <i@mezzp.com>
2018-11-28 15:36:43 +0800

    一些控件的使用

--- WControls/WControls/Form1.Designer.cs
+++ WControls/WControls/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.textName = new System.Windows.Forms.TextBox();
@@ -40,30 +39,33 @@
             this.rbtnM = new System.Windows.Forms.RadioButton();
             this.rbtnF = new System.Windows.Forms.RadioButton();
             this.btnSubmit = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBoxClass = new System.Windows.Forms.GroupBox();
+            this.rbtnClassOne = new System.Windows.Forms.RadioButton();
+            this.rbtnClassTwo = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBoxClub = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBoxClass.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(72, 31);
+            this.label1.Location = new System.Drawing.Point(46, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "姓名：";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(72, 70);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "性别：";
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(72, 108);
+            this.label3.Location = new System.Drawing.Point(45, 158);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(41, 12);
             this.label3.TabIndex = 2;
@@ -72,7 +74,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(72, 142);
+            this.label4.Location = new System.Drawing.Point(45, 192);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(41, 12);
             this.label4.TabIndex = 3;
@@ -80,7 +82,7 @@
             // 
             // textName
             // 
-            this.textName.Location = new System.Drawing.Point(119, 28);
+            this.textName.Location = new System.Drawing.Point(93, 28);
             this.textName.Name = "textName";
             this.textName.Size = new System.Drawing.Size(100, 21);
             this.textName.TabIndex = 4;
@@ -93,7 +95,7 @@
             "数学",
             "英语",
             "c#"});
-            this.checkedListBox1.Location = new System.Drawing.Point(120, 142);
+            this.checkedListBox1.Location = new System.Drawing.Point(93, 205);
             this.checkedListBox1.Name = "checkedListBox1";
             this.checkedListBox1.Size = new System.Drawing.Size(120, 84);
             this.checkedListBox1.TabIndex = 5;
@@ -101,7 +103,7 @@
             // chkSleep
             // 
             this.chkSleep.AutoSize = true;
-            this.chkSleep.Location = new System.Drawing.Point(120, 108);
+            this.chkSleep.Location = new System.Drawing.Point(93, 158);
             this.chkSleep.Name = "chkSleep";
             this.chkSleep.Size = new System.Drawing.Size(48, 16);
             this.chkSleep.TabIndex = 6;
@@ -111,7 +113,7 @@
             // chkGame
             // 
             this.chkGame.AutoSize = true;
-            this.chkGame.Location = new System.Drawing.Point(226, 108);
+            this.chkGame.Location = new System.Drawing.Point(165, 158);
             this.chkGame.Name = "chkGame";
             this.chkGame.Size = new System.Drawing.Size(48, 16);
             this.chkGame.TabIndex = 7;
@@ -121,7 +123,7 @@
             // chkSing
             // 
             this.chkSing.AutoSize = true;
-            this.chkSing.Location = new System.Drawing.Point(325, 108);
+            this.chkSing.Location = new System.Drawing.Point(242, 158);
             this.chkSing.Name = "chkSing";
             this.chkSing.Size = new System.Drawing.Size(48, 16);
             this.chkSing.TabIndex = 8;
@@ -131,7 +133,7 @@
             // rbtnM
             // 
             this.rbtnM.AutoSize = true;
-            this.rbtnM.Location = new System.Drawing.Point(120, 70);
+            this.rbtnM.Location = new System.Drawing.Point(28, 29);
             this.rbtnM.Name = "rbtnM";
             this.rbtnM.Size = new System.Drawing.Size(35, 16);
             this.rbtnM.TabIndex = 9;
@@ -142,7 +144,7 @@
             // rbtnF
             // 
             this.rbtnF.AutoSize = true;
-            this.rbtnF.Location = new System.Drawing.Point(256, 70);
+            this.rbtnF.Location = new System.Drawing.Point(90, 29);
             this.rbtnF.Name = "rbtnF";
             this.rbtnF.Size = new System.Drawing.Size(35, 16);
             this.rbtnF.TabIndex = 10;
@@ -152,7 +154,7 @@
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(120, 267);
+            this.btnSubmit.Location = new System.Drawing.Point(93, 317);
             this.btnSubmit.Name = "btnSubmit";
             this.btnSubmit.Size = new System.Drawing.Size(75, 23);
             this.btnSubmit.TabIndex = 11;
@@ -160,14 +162,109 @@
             this.btnSubmit.UseVisualStyleBackColor = true;
             this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbtnF);
+            this.groupBox1.Controls.Add(this.rbtnM);
+            this.groupBox1.Location = new System.Drawing.Point(47, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(146, 64);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "性别";
+            // 
+            // groupBoxClass
+            // 
+            this.groupBoxClass.Controls.Add(this.rbtnClassOne);
+            this.groupBoxClass.Controls.Add(this.rbtnClassTwo);
+            this.groupBoxClass.Location = new System.Drawing.Point(199, 70);
+            this.groupBoxClass.Name = "groupBoxClass";
+            this.groupBoxClass.Size = new System.Drawing.Size(149, 64);
+            this.groupBoxClass.TabIndex = 13;
+            this.groupBoxClass.TabStop = false;
+            this.groupBoxClass.Text = "班级";
+            // 
+            // rbtnClassOne
+            // 
+            this.rbtnClassOne.AutoSize = true;
+            this.rbtnClassOne.Location = new System.Drawing.Point(17, 29);
+            this.rbtnClassOne.Name = "rbtnClassOne";
+            this.rbtnClassOne.Size = new System.Drawing.Size(47, 16);
+            this.rbtnClassOne.TabIndex = 14;
+            this.rbtnClassOne.TabStop = true;
+            this.rbtnClassOne.Text = "一班";
+            this.rbtnClassOne.UseVisualStyleBackColor = true;
+            // 
+            // rbtnClassTwo
+            // 
+            this.rbtnClassTwo.AutoSize = true;
+            this.rbtnClassTwo.Location = new System.Drawing.Point(84, 29);
+            this.rbtnClassTwo.Name = "rbtnClassTwo";
+            this.rbtnClassTwo.Size = new System.Drawing.Size(47, 16);
+            this.rbtnClassTwo.TabIndex = 15;
+            this.rbtnClassTwo.TabStop = true;
+            this.rbtnClassTwo.Text = "二班";
+            this.rbtnClassTwo.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(396, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 129);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(405, 181);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 15;
+            this.btnUpload.Text = "上传照片";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBoxClub
+            // 
+            this.listBoxClub.FormattingEnabled = true;
+            this.listBoxClub.ItemHeight = 12;
+            this.listBoxClub.Items.AddRange(new object[] {
+            "IT社",
+            "乒乓球社",
+            "轮滑社"});
+            this.listBoxClub.Location = new System.Drawing.Point(293, 228);
+            this.listBoxClub.Name = "listBoxClub";
+            this.listBoxClub.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBoxClub.Size = new System.Drawing.Size(120, 88);
+            this.listBoxClub.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(291, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "社团";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(532, 376);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBoxClub);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBoxClass);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btnSubmit);
-            this.Controls.Add(this.rbtnF);
-            this.Controls.Add(this.rbtnM);
             this.Controls.Add(this.chkSing);
             this.Controls.Add(this.chkGame);
             this.Controls.Add(this.chkSleep);
@@ -175,10 +272,14 @@
             this.Controls.Add(this.textName);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBoxClass.ResumeLayout(false);
+            this.groupBoxClass.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,7 +288,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textName;
@@ -198,6 +298,15 @@
         private System.Windows.Forms.RadioButton rbtnM;
         private System.Windows.Forms.RadioButton rbtnF;
         private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBoxClass;
+        private System.Windows.Forms.RadioButton rbtnClassOne;
+        private System.Windows.Forms.RadioButton rbtnClassTwo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBoxClub;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- WControls/WControls/Form1.cs
+++ WControls/WControls/Form1.cs
@@ -21,23 +21,39 @@ namespace WControls
         {
             string name = textName.Text.Trim();
             string gender;
+            string myclass;
             string hobby="";
             string course="";
-            if (rbtnM.Checked)
+            string club = "";
+            if (rbtnM.Checked)//性别
                 gender = rbtnM.Text;
             else
                 gender = rbtnF.Text;
-            if(chkSleep.Checked)
+            if (rbtnClassOne.Checked)//班级
+                myclass = rbtnClassOne.Text;
+            else
+                myclass = rbtnClassTwo.Text;
+            if(chkSleep.Checked)//爱好
                 hobby+=chkSleep.Text;
             if(chkGame.Checked)
                 hobby+=chkGame.Text;
              if(chkSing.Checked)
                 hobby+=chkSing.Text;
-            foreach(object o in checkedListBox1.CheckedItems)
+            foreach(object o in checkedListBox1.CheckedItems)//课程
             {
                 course+=o.ToString();
             }
-            MessageBox.Show("姓名"+name + "，性别" + gender+"，爱好" + hobby +"，学了"+ course);
+            foreach(object o in listBoxClub.SelectedItems)//社团
+            {
+                club += o.ToString();
+            }
+            MessageBox.Show("姓名"+name + "，性别" + gender+"，班级"+myclass+"，爱好" + hobby +"，学了"+ course+"，报了"+club);
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            pictureBox1.ImageLocation = openFileDialog1.FileName;
         }
     }
 }
