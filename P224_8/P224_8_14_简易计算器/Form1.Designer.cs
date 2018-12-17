3b159a9bc7da9ce425418566d88df24381672ea1
雨心 <i@mezzp.com>
2018-12-17 13:44:57 +0800

    计算器未完成

--- P224_8/P224_8_14_简易计算器/Form1.Designer.cs
+++ P224_8/P224_8_14_简易计算器/Form1.Designer.cs
@@ -43,10 +43,10 @@
             this.btn6 = new System.Windows.Forms.Button();
             this.btn3 = new System.Windows.Forms.Button();
             this.btnDian = new System.Windows.Forms.Button();
-            this.button16 = new System.Windows.Forms.Button();
-            this.button17 = new System.Windows.Forms.Button();
-            this.button18 = new System.Windows.Forms.Button();
-            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
             this.btnEqu = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -74,6 +74,7 @@
             this.btn7.TabIndex = 1;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
             // 
             // btn4
             // 
@@ -83,6 +84,7 @@
             this.btn4.TabIndex = 1;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
             // 
             // btn1
             // 
@@ -92,6 +94,7 @@
             this.btn1.TabIndex = 1;
             this.btn1.Text = "1";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // btn0
             // 
@@ -101,6 +104,7 @@
             this.btn0.TabIndex = 1;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
             // 
             // btn
             // 
@@ -108,7 +112,7 @@
             this.btn.Name = "btn";
             this.btn.Size = new System.Drawing.Size(61, 33);
             this.btn.TabIndex = 1;
-            this.btn.Text = "button1";
+            this.btn.Text = "+/-";
             this.btn.UseVisualStyleBackColor = true;
             // 
             // btn8
@@ -119,6 +123,7 @@
             this.btn8.TabIndex = 1;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
             // 
             // btn5
             // 
@@ -128,6 +133,7 @@
             this.btn5.TabIndex = 1;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
             // 
             // btn2
             // 
@@ -137,6 +143,7 @@
             this.btn2.TabIndex = 1;
             this.btn2.Text = "2";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
             // btn百分号
             // 
@@ -144,7 +151,7 @@
             this.btn百分号.Name = "btn百分号";
             this.btn百分号.Size = new System.Drawing.Size(61, 33);
             this.btn百分号.TabIndex = 1;
-            this.btn百分号.Text = "button1";
+            this.btn百分号.Text = "%";
             this.btn百分号.UseVisualStyleBackColor = true;
             // 
             // btn9
@@ -155,6 +162,7 @@
             this.btn9.TabIndex = 1;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
             // 
             // btn6
             // 
@@ -164,6 +172,7 @@
             this.btn6.TabIndex = 1;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
             // 
             // btn3
             // 
@@ -173,6 +182,7 @@
             this.btn3.TabIndex = 1;
             this.btn3.Text = "3";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // btnDian
             // 
@@ -183,41 +193,45 @@
             this.btnDian.Text = ".";
             this.btnDian.UseVisualStyleBackColor = true;
             // 
-            // button16
+            // btnDiv
             // 
-            this.button16.Location = new System.Drawing.Point(236, 137);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(61, 33);
-            this.button16.TabIndex = 1;
-            this.button16.Text = "button1";
-            this.button16.UseVisualStyleBackColor = true;
+            this.btnDiv.Location = new System.Drawing.Point(236, 137);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(61, 33);
+            this.btnDiv.TabIndex = 1;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
             // 
-            // button17
+            // btnMul
             // 
-            this.button17.Location = new System.Drawing.Point(236, 176);
-            this.button17.Name = "button17";
-            this.button17.Size = new System.Drawing.Size(61, 33);
-            this.button17.TabIndex = 1;
-            this.button17.Text = "button1";
-            this.button17.UseVisualStyleBackColor = true;
+            this.btnMul.Location = new System.Drawing.Point(236, 176);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(61, 33);
+            this.btnMul.TabIndex = 1;
+            this.btnMul.Text = "x";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
             // 
-            // button18
+            // btnMinus
             // 
-            this.button18.Location = new System.Drawing.Point(236, 215);
-            this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(61, 33);
-            this.button18.TabIndex = 1;
-            this.button18.Text = "button1";
-            this.button18.UseVisualStyleBackColor = true;
+            this.btnMinus.Location = new System.Drawing.Point(236, 215);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(61, 33);
+            this.btnMinus.TabIndex = 1;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
             // 
-            // btnAdd
+            // btnPlus
             // 
-            this.btnAdd.Location = new System.Drawing.Point(236, 254);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(61, 33);
-            this.btnAdd.TabIndex = 1;
-            this.btnAdd.Text = "+";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnPlus.Location = new System.Drawing.Point(236, 254);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(61, 33);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
             // 
             // btnEqu
             // 
@@ -227,6 +241,7 @@
             this.btnEqu.TabIndex = 1;
             this.btnEqu.Text = "=";
             this.btnEqu.UseVisualStyleBackColor = true;
+            this.btnEqu.Click += new System.EventHandler(this.btnEqu_Click);
             // 
             // Form1
             // 
@@ -236,19 +251,19 @@
             this.Controls.Add(this.btnEqu);
             this.Controls.Add(this.btnDian);
             this.Controls.Add(this.btn0);
-            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnPlus);
             this.Controls.Add(this.btn3);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
-            this.Controls.Add(this.button18);
+            this.Controls.Add(this.btnMinus);
             this.Controls.Add(this.btn6);
             this.Controls.Add(this.btn5);
             this.Controls.Add(this.btn4);
-            this.Controls.Add(this.button17);
+            this.Controls.Add(this.btnMul);
             this.Controls.Add(this.btn9);
             this.Controls.Add(this.btn8);
             this.Controls.Add(this.btn7);
-            this.Controls.Add(this.button16);
+            this.Controls.Add(this.btnDiv);
             this.Controls.Add(this.btn百分号);
             this.Controls.Add(this.btn);
             this.Controls.Add(this.btnAC);
@@ -277,10 +292,10 @@
         private System.Windows.Forms.Button btn6;
         private System.Windows.Forms.Button btn3;
         private System.Windows.Forms.Button btnDian;
-        private System.Windows.Forms.Button button16;
-        private System.Windows.Forms.Button button17;
-        private System.Windows.Forms.Button button18;
-        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
         private System.Windows.Forms.Button btnEqu;
     }
 }
--- P224_8/P224_8_14_简易计算器/Form1.cs
+++ P224_8/P224_8_14_简易计算器/Form1.cs
@@ -16,6 +16,90 @@ namespace P224_8_14_简易计算器
         {
             InitializeComponent();
         }
+       public double num1;//第一个数
+       public double num2;//第二个数
+       public int operation;//结果
+        public void addNum(int num)
+        {
+            textBox1.Text = textBox1.Text + num.ToString();
+        }
+        private void btnEqu_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            addNum(1);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            addNum(2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            addNum(3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            addNum(4);
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            addNum(0);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            addNum(5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            addNum(6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            addNum(7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            addNum(8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            addNum(9);
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            Num1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "";
+            Operation = 1;
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+
+        }
+
 
     }
 }
