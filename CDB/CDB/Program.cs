d58bfcd23d3518768599b286af99c66fdc1bc060
雨心 <i@mezzp.com>
2018-12-19 15:37:24 +0800

    对数据库进行增删改查

--- CDB/CDB/Program.cs
+++ CDB/CDB/Program.cs
@@ -16,16 +16,49 @@ namespace CDB
             {
                 conn = new SqlConnection(@"Data Source=506-63;Initial Catalog=teaching;Integrated Security=True");
                 conn.Open();
-                Console.WriteLine("数据库连接成功");
-                SqlCommand comm = new SqlCommand("select * from student", conn);
-                SqlDataReader reader = comm.ExecuteReader();
-                if(reader.HasRows)
+                //增
+                //SqlCommand comm = new SqlCommand("insert into student values('s8','哈哈哈','M',24)", conn);
+                //if(comm.ExecuteNonQuery() >0)
+                //{
+                //    Console.WriteLine("插入成功");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("插入失败");
+                //}
+
+                //删
+                //SqlCommand comm = new SqlCommand("delete from student where sno='s8'", conn);
+                //if (comm.ExecuteNonQuery() > 0)
+                //{
+                //    Console.WriteLine("删除成功");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("删除失败");
+                //}
+              
+                //改
+                SqlCommand comm = new SqlCommand("update student set age =21 where sno='s7'", conn);
+                if (comm.ExecuteNonQuery() > 0)
                 {
-                    while(reader.Read())
-                    {
-                        Console.WriteLine("学号：{0}，姓名：{1}，性别：{2}，年龄：{3}", reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetInt16(3));
-                    }
+                    Console.WriteLine("更改成功");
                 }
+                else
+                {
+                    Console.WriteLine("更改失败");
+                }
+
+                //查
+                //SqlCommand comm = new SqlCommand("select * from student", conn);
+                //SqlDataReader reader = comm.ExecuteReader();
+                //if(reader.HasRows)
+                //{
+                //    while(reader.Read())
+                //    {
+                //        Console.WriteLine("学号：{0}，姓名：{1}，性别：{2}，年龄：{3}", reader.GetString(0), reader.GetString(1), reader.GetString(2), reader.GetInt16(3));
+                //    }
+                //}
             }
             catch(Exception ex)
             {
